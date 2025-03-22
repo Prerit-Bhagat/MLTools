85f9b91e785d1cc92c96c990ddd92c06a007a0b9
Prerit-Bhagat <preritbhagat001@gmail.com>
2025-03-23 00:28:28 +0530

    Home Page Updated

--- Tools/src/pages/Home/Home.jsx
+++ Tools/src/pages/Home/Home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import "./Home.css"; 
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="Main">
-      <h3>Find Your Ideal ML Model!</h3>
-      <h1>Revolutionizing </h1>
-      <h1>Technology with Al</h1>
-      <p>Unlock the Power of AI for Machine Learning! Effortlessly discover the best ML algorithms tailored to your data with PyCaret. Optimize predictions, streamline workflows, and elevate data-driven decision-making. Experience the future of automated ML selection today!</p>
+      {/* Left side with text */}
+      <div className="left-content">
+        <h3>Find Your Ideal ML Model!</h3>
+        <h1>Revolutionizing</h1>
+        <h1>Technology with AI</h1>
+        <p>
+          Unlock the Power of AI for Machine Learning! Effortlessly discover the best ML 
+          algorithms tailored to your data with PyCaret. Optimize predictions, streamline 
+          workflows, and elevate data-driven decision-making. Experience the future of 
+          automated ML selection today!
+        </p>
+      </div>
+
+      {/* Right side with background image */}
+      <div className="right-content"><img src="Background.jpg" alt="AIImage" /></div>
     </div>
   );
 };
